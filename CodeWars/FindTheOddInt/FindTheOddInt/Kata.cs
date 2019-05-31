f14198b75ff1a94aa3d1c2fe6ccbb0e78d9316b2
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-05-31 00:51:45 -0400

    Tdd kata to find odd integer

---
+++ CodeWars/FindTheOddInt/FindTheOddInt/Kata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheOddInt
+{
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            Dictionary<int, int> numberDict = new Dictionary<int, int>();
+            int result = 0;
+            for (int i = 0; i < seq.Length; i++)
+            {
+                if (numberDict.ContainsKey(seq[i]))
+                {
+                    numberDict[seq[i]] += 1;
+                }
+                else
+                {
+                    numberDict.Add(seq[i], 1);
+                }
+            }
+
+            foreach (KeyValuePair<int, int> numSeq in numberDict)
+            {
+                if (numSeq.Value % 3 == 0)
+                {
+                    result = numSeq.Key;
+                    Console.WriteLine("result " + result);
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ CodeWars/FindTheOddInt/FindTheOddInt/UnitTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System;
+
+namespace FindTheOddInt
+{
+    public class UnitTests
+    {
+         
+
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void Tests()
+            {
+
+                Assert.AreEqual(5, Kata.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+            }
+        }
+
+    }
+
+    
+}
