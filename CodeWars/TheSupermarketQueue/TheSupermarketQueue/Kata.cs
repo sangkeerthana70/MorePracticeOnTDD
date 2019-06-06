114824fae6cc2ac26843887332a85810a4976e4d
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-05 23:27:08 -0400

    Initial commit

---
+++ CodeWars/TheSupermarketQueue/TheSupermarketQueue/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSupermarketQueue
+{
+    class Kata
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            return 0;
+        }
+    }
+}
