1b33fa6b4c99cd93c7ef539756a3e6bc61a15b39
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-10 16:36:49 -0400

    Added TimeSheetCalculator Kata-passed all test cases

---
+++ TimeSheetCalculatorKata/TimeSheetCalculatorKata/TimeSheetCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSheetCalculatorKata
+{
+    class TimeSheetCalculator
+    {
+        public static float CalculateTime(float start, float breakTime, float end)
+        {
+            if(start != null && end!= null)
+            {
+                if(end > start)
+                {
+                    if (breakTime != 0)
+                    {
+                        return (end - start - breakTime) - 0.40f;
+                    }
+                   
+                    return (end - start - breakTime);
+                }
+                else
+                {
+                    if (breakTime != 0)
+                    {
+                        return ( start - end - breakTime) - 0.40f;
+                    }
+
+                    return (start - end - breakTime) ;
+                }
+            }
+
+            throw new ArgumentException("input was not in correct format");
+        }
+    }
+}
---
+++ TimeSheetCalculatorKata/TimeSheetCalculatorKata/UnitTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSheetCalculatorKata
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void CheckIfInputHasAStartTimeAndAnEndTime()
+        {
+            float startTime = 08.00f;
+            float endTime = 16.00f;
+            float breakTime = 0f;
+
+            var expected = 8.00f;
+
+            var actual = TimeSheetCalculator.CalculateTime(startTime, breakTime, endTime);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SwitchTheOrderOfInputShouldReturnCorrectResult()
+        {
+            float startTime = 16.00f;
+            float endTime = 8.00f;
+            float breakTime = 0f;
+
+            var expected = 8.00f;
+
+            var actual = TimeSheetCalculator.CalculateTime(startTime, breakTime, endTime);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnCorrectResultIfThereIsABreakTime()
+        {
+            float startTime = 07.02f;
+            float endTime = 16.22f;
+            float breakTime = 0f;
+
+            var expected = 9.20f;
+
+            var actual = TimeSheetCalculator.CalculateTime(startTime, breakTime ,endTime);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
