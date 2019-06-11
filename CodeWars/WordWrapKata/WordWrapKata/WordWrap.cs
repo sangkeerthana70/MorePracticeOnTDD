246ac0bc15ebadb4f453520727c78e918ac89599
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-11 16:27:34 -0400

    Initial commit

---
+++ CodeWars/WordWrapKata/WordWrapKata/WordWrap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordWrapKata
+{
+   public  class WordWrap
+    {
+        public static String wrap(String text, int limit)
+        {
+            for(int index = 0; index < text.Length; index++)
+            {
+                if (Char.IsPunctuation(text[index]))
+                {
+                    return "Invalid input string has punctuation";
+                }
+            }
+            
+            return "no punctuations";
+        }
+    }
+}
---
+++ CodeWars/WordWrapKata/WordWrapKata/WordWrapTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordWrapKata
+{
+    [TestFixture]
+    public class WordWrapTests
+    {
+        [Test]
+        public void CheckForInValidInputsAndThrowException()
+        {
+            string input = "Check for punctuation";
+            int lineLimit = 7;
+            string expected = "no punctuations";
+
+            Assert.AreEqual(expected, WordWrap.wrap(input, lineLimit));
+        }
+    }
+}
