64d85866f5c24abc6ed889e6ccfc54c030dce16c
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-13 11:05:07 -0400

    refactored code and passed all test cases

--- CodeWars/BitCounting/BitCounting/BitCounting.cs
+++ CodeWars/BitCounting/BitCounting/BitCounting.cs
@@ -11,73 +11,27 @@ namespace BitCounting
         public static int countBits(int n)
         {
             //throw new NotImplementedException();
-            //string binary = Convert.ToString(n, 2);
-            //Console.WriteLine("binary " + binary);
-
-            if(n == 0)
+            if (n == 0)
             {
                 return 0;
             }
-
-            
-            List<int> result = new List<int>();
-            int quotient = n;
-            int remainder = 0;
-                
-            while (quotient != 0)
-            {
-                // Continue dividing the quotient by 2 until you get a quotient of zero
-                remainder = quotient % 2;
-                Console.WriteLine("remainder: " + remainder);
-                quotient = quotient / 2;
-                result.Add(remainder);
-            }
-            Console.WriteLine("result: " + String.Join(",", result));
-            int numOfOnes = 0;
-            string binary = string.Empty;
-            for (var i = 0; i < result.Count; i++)
-            {
-                // reverses the elements in result array
-                binary += result[result.Count - 1 - i];
-                Console.WriteLine("binary: " + binary);
-            }
+            // Convert the number t0 a string given a base of 2 which represents a binary
+            string binary = Convert.ToString(n, 2);
+            Console.WriteLine("binary " + binary);
             return CheckConsecutiveOnes(binary);
-
         }
 
         private static int CheckConsecutiveOnes(string binary)
         {
-            //binary = "11101111";
-            //Console.WriteLine(binary);
             int count = 0;
-            int maxCount = 0;
-
             for (var i = 0; i < binary.Length; i++)
             {
-                Console.WriteLine("i: " + i);
-                Console.WriteLine("binary[i]: " + binary[i]);
-                Console.WriteLine(binary[i].GetType());
-                Console.WriteLine(binary[i] == '1');
                 if (binary[i] == '1')
                 {
                     count += 1;
                 }
-                // if(binary[i] == '0'
-                else
-                {
-                    count = 0;
-                }
-
-                if (count > maxCount)
-                {
-                    maxCount = count;
-                }
-                //Console.WriteLine("count: " + count);
-                //Console.WriteLine("maxCount: " + maxCount);
             }
-
-            //Console.WriteLine("count: " + maxCount);
-            return maxCount;
+            return count;
         }
     }
 }
--- CodeWars/BitCounting/BitCounting/UnitTests.cs
+++ CodeWars/BitCounting/BitCounting/UnitTests.cs
@@ -34,6 +34,37 @@ namespace BitCounting
             int expected = 3;
             Assert.AreEqual(expected, BitCounting.countBits(input));
         }
-       
+
+        [Test]
+        public void Inputof9Returns2Ones_InBinaryRepresentationOf9()
+        {
+            int input = 9;
+            int expected = 2;
+            Assert.AreEqual(expected, BitCounting.countBits(input));
+        }
+
+        [Test]
+        public void Inputof10Returns2Ones_InBinaryRepresentationOf10()
+        {
+            int input = 10;
+            int expected = 2;
+            Assert.AreEqual(expected, BitCounting.countBits(input));
+        }
+        [Test]
+        public void Inputof1234Returns5Ones_InBinaryRepresentationOf1234()
+        {
+            int input = 1234;
+            int expected = 5;
+            Assert.AreEqual(expected, BitCounting.countBits(input));
+        }
+
+        [Test]
+        public void Inputof1500Returns7Ones_InBinaryRepresentationOf1500()
+        {
+            int input = 1500;
+            int expected = 7;
+            Assert.AreEqual(expected, BitCounting.countBits(input));
+        }
+
     }
 }
