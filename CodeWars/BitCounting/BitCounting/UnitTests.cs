87b4a688b8cba313b129661c16fd93625d6bcc12
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-12 23:47:06 -0400

    Initial commit still working on the logic

---
+++ CodeWars/BitCounting/BitCounting/UnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCounting
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void CheckIfInputReturnsExpectedOutput()
+        {
+            int input = 0;
+            int expected = 0;
+            Assert.AreEqual(expected, BitCounting.countBits(input));
+        }
+
+        [Test]
+        public void Inputof1Returns4Ones_InBinaryRepresentationOf1()
+        {
+            int input = 1;
+            int expected = 4;
+            Assert.AreEqual(expected, BitCounting.countBits(input));
+        }
+    }
+}
