425bda416817b682db6ee5b6ad11a184f1509e14
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-17 16:25:40 -0400

    Initial commit- passed two test cases so far

---
+++ CodeWars/FindTheStrayNumber/FindTheStrayNumber/StrayNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheStrayNumber
+{
+    public class StrayNumbers
+    {
+        public static int Stray(int[] numbers)
+        {
+            
+            int result = 0;
+            IEnumerable<int> strayNumber = numbers.Distinct();
+            //foreach(int num in numbers)
+            //{
+            //    result = num;
+            //    Console.WriteLine("stray num " + num);
+            //}
+            for(int i = 0; i < numbers.Length -1; i++)
+            {
+                if(numbers[i] != numbers[i + 1])
+                {
+                    Console.WriteLine("unique: " + numbers[i+1]);
+                    result = numbers[i + 1];
+                    break;
+                }
+            }
+            return result;
+            
+
+        }
+    }
+   
+}
---
+++ CodeWars/FindTheStrayNumber/FindTheStrayNumber/UnitTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheStrayNumber
+{
+    [TestFixture]
+    class UnitTests
+    {
+        [Test]
+        public void CheckIfAInputReturnsTheExpectedOutput()
+        {
+
+            int expected = 0;
+            Assert.AreEqual(0, StrayNumbers.Stray(new int[] { 0, 0, 0 }));
+        }
+        
+        [Test]
+        public void AnArrayOfLength3Returns2AsOutput()
+        {
+            Assert.AreEqual(2, StrayNumbers.Stray(new int[] { 1, 1, 2 }));
+        }
+
+        [Test]
+        public void AnArrayOfOddInputReturns3AsOutput()
+        {
+            Assert.AreEqual(3, StrayNumbers.Stray(new int[] { 17, 17, 3, 17, 17, 17, 17 }));
+        }
+
+    }
+}
