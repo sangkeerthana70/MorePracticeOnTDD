7895b93d3645e1af38b8d3799f0da76547c2ff77
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-18 13:39:37 -0400

    git commit -m initial commit

---
+++ WhiteSpaceTrim/Services/TrimSpaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TrimSpaces
+    {
+        public static string CheckForWhiteSpace(string input)
+        {
+            
+            char[] charsToRemove = { ' ', '\t' };
+            if (input == null)
+                throw new NullReferenceException();
+
+            if (input == string.Empty)
+                throw new ArgumentException();
+
+            string[] words = input.Split(' ');
+            foreach(string word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    word.TrimEnd(charsToRemove);
+                }
+                
+            }
+            
+
+            //return  input.Replace(" ", "");
+
+
+            return string.Join("",words);//input.TrimEnd(charsToRemove);
+            
+            
+        }
+    }
+}
---
+++ WhiteSpaceTrim/WhiteSpaceTrim/UnitTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteSpaceTrim
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+
+        public void CheckIfAStringIsNull_ReturnException()
+        {
+            string input = null;
+            Assert.Throws<System.NullReferenceException>(() => TrimSpaces.CheckForWhiteSpace(input));
+        }
+
+        [Test]
+
+        public void CheckIfAStringIsEmpty_ReturnException()
+        {
+            string input = "";
+            Assert.Throws<System.ArgumentException>(() => TrimSpaces.CheckForWhiteSpace(input));
+        }
+
+        [Test]
+        
+        public void CheckForNoWhitespace_InAString()
+        {
+            string input = "abc";
+            string expected = "abc";
+            var actual = TrimSpaces.CheckForWhiteSpace(input);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        
+        public void RemoveWhiteSpacesAtEndOfString()
+        {
+            string input = "abc    ";
+            string expected = "abc";
+            var actual = TrimSpaces.CheckForWhiteSpace(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        
+        public void KeepsWhitespaceAtBeginningOfString()
+        {
+            string input = "   abc    ";
+            string expected = "   abc";
+            var actual = TrimSpaces.CheckForWhiteSpace(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckForTabsAndRemoveThemFromString()
+        {
+            string input = "abc\t ";
+            string expected = "abc";
+            var actual = TrimSpaces.CheckForWhiteSpace(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RemoveWhiteSpaceInMiddleOfString()
+        {
+            string input = "abc\r\n cd \r\n ";
+            string expected = "abc\r\ncd\r\n";
+            var actual = TrimSpaces.CheckForWhiteSpace(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
