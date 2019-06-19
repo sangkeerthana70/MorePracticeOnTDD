944f8ea5517c74bcde4201cf2fa6f9f75bfcd6bb
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-18 23:46:03 -0400

    Initial commit

---
+++ GreetingKata/GreetingKata/UnitTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using GreetingServices;
+
+
+namespace GreetingKata
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        Greeting greeting = new Greeting();
+
+        [Test]
+        public void GreetASinglePersonByTheirName()
+        {
+            string name = "Bob";
+            string expected = "Hello, Bob.";
+            string actual = greeting.GreetName(name);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void GreetNameShouldHandleNullIfNameIsNull()
+        {
+            string name = "";
+            string expected = "Hello, my friend.";
+            string actual = greeting.GreetName(name);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ GreetingKata/GreetingServices/Greeting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingServices
+{
+    public  class Greeting
+    {
+        public string GreetName(string name)
+        {
+            if(name == "Bob")
+            {
+                return "Hello, Bob.";
+            }
+
+            if(name == null)
+            {
+                return "Hello, my friend.";
+
+            }
+
+            return name;
+        }
+
+
+    }
+}
