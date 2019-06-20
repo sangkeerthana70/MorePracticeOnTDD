5d338b0ea50fc74c552414749763d2354022e54a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-19 23:14:35 -0400

    Initial commit

---
+++ CodeWars/TheHighestProfitWins/TheHighestProfitWins/MinMax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHighestProfitWins
+{
+    public class MinMax
+    {
+        public static int[] minMax(int[] lst)
+        {
+            List<int> minAndMaxNum = new List<int>();
+            minAndMaxNum.Add(lst.Min());
+            
+            minAndMaxNum.Add(lst.Max());
+
+            foreach(int num in minAndMaxNum)
+                Console.WriteLine("num: " + num);
+            return minAndMaxNum.ToArray();
+        }
+    }
+}
---
+++ CodeWars/TheHighestProfitWins/TheHighestProfitWins/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHighestProfitWins
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public static void BasicTest()
+        {
+            Assert.AreEqual(new int[] { -1, 20 }, MinMax.minMax(new int[] { 1, 2, 5, -1, 12, 20 }));
+            //Assert.AreEqual(new int[] { 1, 5 }, MinMax.minMax(new int[] { 1, 2, 3, 4, 5 }));
+           // Assert.AreEqual(new int[] { -3, 5 }, MinMax.minMax(new int[] { 1, 2, -3, 4, 5 }));
+        }
+    }
+}
