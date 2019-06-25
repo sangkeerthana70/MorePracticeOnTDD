b6ba31941d20dd2fc4a0668685f7cabd654ccc2b
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-25 11:07:10 -0400

    initial commit passed first test case

---
+++ CodeWars/ConvertStringToCamelCase/ConvertStringToCamelCase/ConvertSTringToCamelCaseTdd.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertStringToCamelCase
+{
+    [TestFixture]
+    public class ConvertSTringToCamelCaseTdd
+    {
+        [Test]
+        public void InputStringWithDashesInbetween_ShouldBeRemovedFromTheOutput()
+        {
+            string input = "the-Stealth-Warrior";
+            string expected = "theStealthWarrior";
+
+            string actual = Kata.ToCamelCase(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CodeWars/ConvertStringToCamelCase/ConvertStringToCamelCase/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConvertStringToCamelCase
+{
+    public class Kata
+    {
+        public static string ToCamelCase(string str)
+        {
+            //string solution = string.Concat(str.Where(i => !new[] { '.', ' ', '-', '(', ')' }.Contains(i)));
+            string solution1 = Regex.Replace(str, "-", "");
+
+            Console.WriteLine("solution: " + solution1);
+            return solution1;
+        } 
+    }
+}
