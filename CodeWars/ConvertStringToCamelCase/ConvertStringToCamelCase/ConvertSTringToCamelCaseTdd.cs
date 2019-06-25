9862f7792314d4e49ba079faff3b812dc9e97713
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-25 13:34:08 -0400

    solved

--- CodeWars/ConvertStringToCamelCase/ConvertStringToCamelCase/ConvertSTringToCamelCaseTdd.cs
+++ CodeWars/ConvertStringToCamelCase/ConvertStringToCamelCase/ConvertSTringToCamelCaseTdd.cs
@@ -10,14 +10,34 @@ namespace ConvertStringToCamelCase
     [TestFixture]
     public class ConvertSTringToCamelCaseTdd
     {
+ 
         [Test]
-        public void InputStringWithDashesInbetween_ShouldBeRemovedFromTheOutput()
+       
+        public void InputOfDashDelimitedWords_ShouldRemoveDashesAndConvertToCamelCase()
         {
-            string input = "the-Stealth-Warrior";
+            string input = "the-stealth-warrior";
             string expected = "theStealthWarrior";
 
             string actual = Kata.ToCamelCase(input);
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+
+        public void InputOfUnderscoreDelimitedWords_ShouldRemoveDashesAndConvertToCamelCase()
+        {
+            string input = "the_rain_maker";
+            string expected = "theRainMaker";
+
+            string actual = Kata.ToCamelCase(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+       
+        [Test]
+        public void FirstWordIntheOutputShouldBeCapitalized_OnlyIfOriginalWordWasCapitalizedToPreservePascalCase()
+        {
+            Assert.AreEqual("ThreeMenInABoat", Kata.ToCamelCase("Three_Men_In_A_Boat"));
         }
     }
 }
--- CodeWars/ConvertStringToCamelCase/ConvertStringToCamelCase/Kata.cs
+++ CodeWars/ConvertStringToCamelCase/ConvertStringToCamelCase/Kata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -11,11 +12,36 @@ namespace ConvertStringToCamelCase
     {
         public static string ToCamelCase(string str)
         {
-            //string solution = string.Concat(str.Where(i => !new[] { '.', ' ', '-', '(', ')' }.Contains(i)));
-            string solution1 = Regex.Replace(str, "-", "");
+            /*
+            //using String.Concat
+            
+            string solution = string.Concat(str.Where(i => !new[] { '.', ' ', '-', '&','(', ')' }.Contains(i)));
+            
+            // using Regex
+            //string solution1 = Regex.Replace(str, "[-.()&_]+", "");
+            */
 
-            Console.WriteLine("solution: " + solution1);
-            return solution1;
-        } 
+            
+            char[] delimiter = { '-', '_' };
+
+            string[] words = str.Split(delimiter);
+
+
+
+
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+            string output = words[0];
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                output += myTI.ToTitleCase(words[i + 1]);
+                Console.WriteLine("\"{0}\" to titlecase: {1}", words[i + 1], myTI.ToTitleCase(words[i + 1]));
+            }
+            Console.WriteLine("output: " + output);
+            return output;
+        }
+
+
     }
+
 }
+
