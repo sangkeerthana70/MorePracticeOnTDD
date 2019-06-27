461cb2e8fd825b33ec6979bfe146497db656e35c
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-06-27 12:24:14 -0400

    convert roman numerals to numbers Tdd -passed initial tests

---
+++ RomanNumeralsKata/RomanNumerals/ConvertRomanToDecimals.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class ConvertRoman
+    {
+        public int ConvertRomanToNumbers(string romanNumeral)
+        {
+            if (romanNumeral == null)
+                throw new ArgumentNullException();
+            else if (romanNumeral == "")
+                throw new ArgumentException();
+            int n = RomanToNumbers(romanNumeral);
+            Console.WriteLine("n : " + n);
+            return n;
+            
+            
+        }
+
+        private int RomanToNumbers(string rNumeral)
+        {
+            int number = 0;
+
+            switch (rNumeral)
+            {
+                case "I":
+                    number = 1;
+                    break;
+
+                case "V":
+                    number = 5;
+                    break;
+                case "X":
+                    number = 10;
+                    break;
+                case "L":
+                    number = 50;
+                    break;
+                case "C":
+                    number = 100;
+                    break;
+                case "D":
+                    number = 500;
+                    break;
+                case "M":
+                    number = 1000;
+                    break;
+            }
+
+            return number;
+
+        }
+
+
+    }
+}
---
+++ RomanNumeralsKata/RomanNumeralsKata/UnitTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        ConvertRoman numbers = new ConvertRoman();
+        [Test]
+        public void CheckIfInputIsEmpty_ThrowsArgumentNullException()
+        {
+            string input = "";
+            Assert.Throws<ArgumentException>(()=> numbers.ConvertRomanToNumbers(input));
+        }
+
+        [Test]
+        public void CheckIfInputIsNull_ThrowsArgumentNullException()
+        {
+            string input = null;
+            Assert.Throws<ArgumentNullException>(() => numbers.ConvertRomanToNumbers(input));
+        }
+
+        [Test]
+        public void InputOfRomanNumeralI_ReturnsOne()
+        {
+            string input = "I";
+            Assert.AreEqual(1, numbers.ConvertRomanToNumbers(input));
+        }
+    }
+}
