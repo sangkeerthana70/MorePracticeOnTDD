c0e53b278910f171de1a47ea81ea630e1cc93f70
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-02 13:27:22 -0400

    Initial set up of Kata

---
+++ CodeWars/MovingZeroesToTheEnd/MovingZeroesToTheEnd/Kata.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingZeroesToTheEnd
+{
+    public static class Kata
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            // TODO: Program me
+            List<int> tempList = new List<int>();
+
+            int zerosCount = 0;
+            
+
+            if(arr.Length == 0)
+                throw new ArgumentException();
+
+            for(int index = 0; index < arr.Length; index++)
+            {
+                if(arr[index] != 0)
+                {
+                    tempList.Add(arr[index]);
+                }
+                else
+                {
+                    zerosCount += 1;
+                }
+            }
+            Console.WriteLine("list count " + tempList.Count());
+            Console.WriteLine("before adding zeros");
+            foreach(int nonZero in tempList)
+            {
+                Console.WriteLine("nonZero " + nonZero);
+            }
+
+            List<int> numberOfZeros = new List<int>(zerosCount);
+            tempList.AddRange(numberOfZeros);
+            Console.WriteLine("after moving zeros");
+            foreach(int num in tempList)
+            {
+                Console.WriteLine("num " + num);
+            }
+            return tempList.ToArray();
+        }
+    }
+}
---
+++ CodeWars/MovingZeroesToTheEnd/MovingZeroesToTheEnd/MovingZerosUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* Write an algorithm that takes an array and moves all of the zeros to the end, 
+ * preserving the order of the other elements.
+*/ 
+
+namespace MovingZeroesToTheEnd
+{
+    [TestFixture]
+    public class MovingZerosUnitTests
+    {
+        [Test]
+        public void IfArrayIsEmpty_ThrowException()
+        {
+            int[] input = new int[] { };
+
+            Assert.Throws<System.ArgumentException>(() => Kata.MoveZeroes(input));
+        }
+
+        [Test]
+        public void ArrayWithOneZeroWithOtherNumbers_MovesZeroToTheEnd()
+        {
+            int[] arr = new int[] { 1, 2, 0, 3, 4 };
+            int[] expected = { 1, 2, 3, 4, 0 };
+            int[] actual = Kata.MoveZeroes(arr);
+            Console.WriteLine("actual " + String.Join("," ,actual));
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
