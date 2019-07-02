8cc2bd2064e520a12e5fc004b8415ea4ea9e4d91
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-01 22:08:56 -0400

    added my solution

---
+++ CodeWars/WriteNumberInExpandedForm/WriteNumberInExpandedForm/Kata.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteNumberInExpandedForm
+{
+    public static class Kata
+    {
+        public static string ExpandedForm(long num)
+        {
+            string numToStr = num.ToString();
+            
+            /*
+            if (numToStr.Length == 1)
+                return numToStr;
+            if (numToStr.Length == 2)
+                return numToStr[0] + "0 + " + numToStr[1];
+            if (numToStr.Length == 3)
+                return numToStr[0] + "00 + " + numToStr[1] + "0 + " + numToStr[2];
+            */
+
+            StringBuilder expandedForm = new StringBuilder();
+            int strLength = numToStr.Length -1;
+
+            //if(numToStr.Length > 3)
+            //{
+                for(int index = 0; index <= strLength; index++)
+                {
+                    if (numToStr[index] == '0')
+                        continue;
+                   
+
+                    if (index == strLength)
+                    {
+                        expandedForm.Append(numToStr[index]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("index " + index);
+                        expandedForm.Append(numToStr[index]).Append('0', strLength - index).Append(" + ");
+                        Console.WriteLine("{0}", expandedForm);
+                    }
+                    
+                    
+                    
+                }
+            return expandedForm.ToString().TrimEnd(' ').Trim('+').TrimEnd(' ');
+            //}
+
+
+            //return numToStr;
+        }
+    }
+}
---
+++ CodeWars/WriteNumberInExpandedForm/WriteNumberInExpandedForm/UnitTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteNumberInExpandedForm
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void InputOfOneDigitNumber_ReturnsNumberOfOnes()
+        {
+            Assert.That(Kata.ExpandedForm(1), Is.EqualTo("1"));
+        }
+
+
+        [Test]
+        public void InputOfTwoDigitNumber_ReturnsNumberOfTensAndOnes()
+        {
+            Assert.That(Kata.ExpandedForm(12), Is.EqualTo("10 + 2"));
+        }
+
+        [Test]
+        public void InputOfThreeDigitNumber_ReturnsNumberOfHundredsTensAndOnes()
+        {
+            Assert.That(Kata.ExpandedForm(125), Is.EqualTo("100 + 20 + 5"));
+        }
+
+        [Test]
+        public void InputOfANumberGreaterThanThreeDigits_ReturnsExpectedOutput()
+        {
+            Assert.That(Kata.ExpandedForm(3479), Is.EqualTo("3000 + 400 + 70 + 9"));
+        }
+        [Test]
+        public void IfLengthIsMoreThanThreeAndNoZeros_ReturnHundredsTensAndOnes()
+        {
+            //long number = 54213;
+            string expected = "50000 + 4000 + 200 + 10 + 3";
+            string actual = Kata.ExpandedForm(54213);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfLengthIsMoreThanThreeAndHasZeros_ReturnHundredsTensAndOnes()
+        {
+            //long number = 54203;
+            string expected = "50000 + 4000 + 200 + 3";
+            string actual = Kata.ExpandedForm(54203);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfOnlyFirstDigitIsANumberNotEqualToZero_ReturnExpectedOutput()
+        {
+            //long number = 9000000;
+            string expected = "9000000";
+            string actual = Kata.ExpandedForm(9000000);
+            Assert.AreEqual(expected, actual);
+        }
+        
+
+    }
+}
