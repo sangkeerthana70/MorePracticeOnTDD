a40028f47893cc246409b2f4fbe5722cefb3c3a8
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-06 18:01:59 -0400

    Set up of kata and tests- passed first few test cases

---
+++ CodeWars/StopgninnipSMysdroW/StopgninnipSMysdroW/StopSpinningUnittests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopgninnipSMysdroW
+{
+    [TestFixture]
+    public class StopSpinningUnittests
+    {
+       
+        [Test]
+        public void CheckIfInputString_IsEmpty()
+        {
+            string input = "";
+            Assert.Throws <ArgumentException>(() => kata.SpinWords(input));
+        }
+
+        [Test]
+        public void CheckIfInputString_IsNull()
+        {
+            string input = null;
+            Assert.Throws<System.NullReferenceException>(() => kata.SpinWords(input));
+        }
+
+        [Test]
+        public void InputOfOneWordWithFiveOrMoreLetter_ReversesTheWord()
+        {
+            string input = "Welcome";
+            string expected = "emocleW";
+            string actual = kata.SpinWords(input);
+            Console.WriteLine("actual " + actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
---
+++ CodeWars/StopgninnipSMysdroW/StopgninnipSMysdroW/kata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopgninnipSMysdroW
+{
+    public class kata
+    {
+        public static string SpinWords(string sentence)
+        {
+            if (sentence == string.Empty)
+            {
+                throw new System.ArgumentException();
+            }
+
+            if (sentence == null)
+            {
+                throw new System.NullReferenceException();
+            }
+
+            if(sentence.Length >= 5)
+            {
+                char[] charArray = sentence.ToCharArray();
+                Console.WriteLine("charArray "+ String.Join(",", charArray));
+                Array.Reverse(charArray);
+
+                return new string(charArray);
+            }
+
+            return sentence;
+        }
+    }
+}
