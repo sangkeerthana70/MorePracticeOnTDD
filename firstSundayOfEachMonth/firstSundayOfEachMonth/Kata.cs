4971166d1726ec881b9117d79fe17e18110d6636
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-09 02:17:12 -0400

    find first Monday of each month

---
+++ firstSundayOfEachMonth/firstSundayOfEachMonth/Kata.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstSundayOfEachMonth
+{
+    public class Kata
+    {
+        public static List<DateTime> FindFirstMondayOfMonth(int year)
+        {
+            List<DateTime> firstMondays = new List<DateTime>();
+            DateTime firstDay = new DateTime(year, 1, 1);
+            Console.WriteLine("date " + firstDay);
+            Console.WriteLine("firstDay.DayOfWeek " + firstDay.DayOfWeek);
+
+            DateTime firstMonday = firstDay;
+            while(firstMonday.DayOfWeek != DayOfWeek.Monday)
+            {
+                firstMonday = firstMonday.AddDays(1);
+               
+                Console.WriteLine("firstMonday " + firstMonday);
+            }
+
+            DateTime date = firstMonday.Date;
+            int firstMondayMonth = firstMonday.Month;
+            int currentYear = firstMonday.Year;
+
+            DateTime currentMonday = firstMonday;
+            firstMondays.Add(currentMonday);
+            
+            DateTime previousMonday;
+
+            while (true)
+            {
+                previousMonday = currentMonday;
+
+                currentMonday = currentMonday.AddDays(7);
+                if (currentMonday.Year != year)
+                {
+                    break;
+                }
+
+                if (previousMonday.Month != currentMonday.Month)
+                {
+                    
+                    firstMondays.Add(currentMonday);
+                }
+
+                
+            }
+
+            foreach(var monday in firstMondays)
+            {
+                Console.WriteLine("monday " + monday);
+            }
+            return firstMondays;
+        }
+    }
+}
---
+++ firstSundayOfEachMonth/firstSundayOfEachMonth/UnitTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstSundayOfEachMonth
+{
+
+    // Program to find the first monday of each month
+
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void InputOfYear2019Returns_ExpectedListOfFirstMondays()
+        {
+            int year = 2019;
+            List<DateTime> expected = new List<DateTime>();
+            expected.Add(new DateTime(2019, 1, 7));
+            expected.Add(new DateTime(2019, 2, 4));
+            expected.Add(new DateTime(2019, 3, 4));
+            expected.Add(new DateTime(2019, 4, 1));
+            expected.Add(new DateTime(2019, 5, 6));
+            expected.Add(new DateTime(2019, 6, 3));
+            expected.Add(new DateTime(2019, 7, 1));
+            expected.Add(new DateTime(2019, 8, 5));
+            expected.Add(new DateTime(2019, 9, 2));
+            expected.Add(new DateTime(2019, 10, 7));
+            expected.Add(new DateTime(2019, 11, 4));
+            expected.Add(new DateTime(2019, 12, 2));
+
+            Assert.AreEqual(expected, Kata.FindFirstMondayOfMonth(year));
+        }
+
+    }
+}
