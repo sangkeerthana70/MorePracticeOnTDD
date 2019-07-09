318b1963881e57e01bfe0d17c965d9cb9e4ea96a
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-09 16:18:35 -0400

    passed all test cases

--- firstSundayOfEachMonth/firstSundayOfEachMonth/Kata.cs
+++ firstSundayOfEachMonth/firstSundayOfEachMonth/Kata.cs
@@ -11,11 +11,13 @@ namespace firstSundayOfEachMonth
         public static List<DateTime> FindFirstMondayOfMonth(int year)
         {
             List<DateTime> firstMondays = new List<DateTime>();
+            // convert the input into a date time object
             DateTime firstDay = new DateTime(year, 1, 1);
             Console.WriteLine("date " + firstDay);
             Console.WriteLine("firstDay.DayOfWeek " + firstDay.DayOfWeek);
 
             DateTime firstMonday = firstDay;
+            // add daysuntil we get first Monday
             while(firstMonday.DayOfWeek != DayOfWeek.Monday)
             {
                 firstMonday = firstMonday.AddDays(1);
@@ -24,24 +26,28 @@ namespace firstSundayOfEachMonth
             }
 
             DateTime date = firstMonday.Date;
+            Console.WriteLine("firstMonday.Date " + date);
             int firstMondayMonth = firstMonday.Month;
+            Console.WriteLine("firstMondayMonth " + firstMondayMonth);
             int currentYear = firstMonday.Year;
 
             DateTime currentMonday = firstMonday;
             firstMondays.Add(currentMonday);
             
             DateTime previousMonday;
-
+            // add all mondays in a year until the year changes to next year
             while (true)
             {
                 previousMonday = currentMonday;
+                Console.WriteLine("previousMonday " + previousMonday);
 
                 currentMonday = currentMonday.AddDays(7);
+                Console.WriteLine("currentMonday " + currentMonday);
                 if (currentMonday.Year != year)
                 {
                     break;
                 }
-
+                // if there is difference in month then add the mondays to the list
                 if (previousMonday.Month != currentMonday.Month)
                 {
                     
--- firstSundayOfEachMonth/firstSundayOfEachMonth/UnitTests.cs
+++ firstSundayOfEachMonth/firstSundayOfEachMonth/UnitTests.cs
@@ -34,5 +34,48 @@ namespace firstSundayOfEachMonth
             Assert.AreEqual(expected, Kata.FindFirstMondayOfMonth(year));
         }
 
+        [Test]
+        public void InputOf2020LeapYear_ReturnsExpectedListOfFirstMondays()
+        {
+            int year = 2020;
+            List<DateTime> expected = new List<DateTime>();
+            expected.Add(new DateTime(2020, 1, 6));
+            expected.Add(new DateTime(2020, 2, 3));
+            expected.Add(new DateTime(2020, 3, 2));
+            expected.Add(new DateTime(2020, 4, 6));
+            expected.Add(new DateTime(2020, 5, 4));
+            expected.Add(new DateTime(2020, 6, 1));
+            expected.Add(new DateTime(2020, 7, 6));
+            expected.Add(new DateTime(2020, 8, 3));
+            expected.Add(new DateTime(2020, 9, 7));
+            expected.Add(new DateTime(2020, 10, 5));
+            expected.Add(new DateTime(2020, 11, 2));
+            expected.Add(new DateTime(2020, 12, 7));
+
+
+            Assert.AreEqual(expected, Kata.FindFirstMondayOfMonth(year));
+
+        }
+
+        [Test]
+        public void InputOf2032_ReturnsExpectedResult()
+        {
+            int year = 2032;
+            List<DateTime> expected = new List<DateTime>();
+            expected.Add(new DateTime(2032, 1, 5));
+            expected.Add(new DateTime(2032, 2, 2));
+            expected.Add(new DateTime(2032, 3, 1));
+            expected.Add(new DateTime(2032, 4, 5));
+            expected.Add(new DateTime(2032, 5, 3));
+            expected.Add(new DateTime(2032, 6, 7));
+            expected.Add(new DateTime(2032, 7, 5));
+            expected.Add(new DateTime(2032, 8, 2));
+            expected.Add(new DateTime(2032, 9, 6));
+            expected.Add(new DateTime(2032, 10, 4));
+            expected.Add(new DateTime(2032, 11, 1));
+            expected.Add(new DateTime(2032, 12, 6));
+
+            Assert.AreEqual(expected, Kata.FindFirstMondayOfMonth(year));
+        }
     }
 }
