ca56a94cee3aea1cbbf00ea9fc9e5c7a219f2f78
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-08 21:07:17 -0400

    Fixed logic to find last Sunday of each month for a given year

---
+++ LastSundayOfEachMonth/LastSunday/Services.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastSunday
+{
+    public class Services
+    {
+        
+        public static List<DateTime> FindLastSundayOfEachMonth(int year)
+        {
+            List<DateTime> sundays = new List<DateTime>();
+
+            Dictionary<int, DateTime> lastSundays = new Dictionary<int, DateTime>();
+            // convert the input into a date time object
+            DateTime firstDay = new DateTime(year, 1, 1);
+            Console.WriteLine("firstDay " + firstDay);
+            Console.WriteLine("firstDay.DayOfWeek " + firstDay.DayOfWeek);
+            DateTime firstSunday = firstDay;
+            while (firstSunday.DayOfWeek != DayOfWeek.Sunday)
+            {
+                firstSunday = firstSunday.AddDays(1);
+                Console.WriteLine("Day of Week " + firstSunday.DayOfWeek);
+
+            }
+
+            DateTime date = firstSunday.Date;
+            Console.WriteLine("year" + date);
+            Console.WriteLine(firstSunday.Month);
+            Console.WriteLine(firstSunday.Year);
+
+
+
+            
+            DateTime currentSunday = firstSunday;
+
+            DateTime previousSunday; 
+
+            while (true)
+            {
+
+                previousSunday = currentSunday;
+
+                currentSunday = currentSunday.AddDays(7);
+                // method-1 check each sunday with previous sunday(before adding 7 days) to get the last Sunday
+                if (previousSunday.Month != currentSunday.Month)
+                {
+                    Console.WriteLine("adding to list");
+                    sundays.Add(previousSunday);
+                }
+
+
+                if (currentSunday.Year != year)
+                {
+                    break;
+                }
+
+                // method-2 Create a Dictionary with month as Key and CurrentSunday as Value.
+                // 
+                if (lastSundays.ContainsKey(currentSunday.Month))
+                {
+                    //update Dictionary[key] = value;
+                    lastSundays[currentSunday.Month] = currentSunday;  
+                }
+                else
+                {
+                    //add key and value
+                    lastSundays.Add(currentSunday.Month, currentSunday);
+                }
+               
+
+                Console.WriteLine("currentSunday " + currentSunday);
+                Console.WriteLine("currentSunday year " + currentSunday.Year);
+                Console.WriteLine("currentMonth " + currentSunday.Month);
+                               
+                Console.WriteLine("========================================================");
+
+            }
+
+
+          foreach(KeyValuePair <int, DateTime> item in lastSundays)
+          {
+                Console.WriteLine("Item " + item);
+                //sundays.Add(item.Value);
+          }
+           return sundays;
+        }
+    }
+}
---
+++ LastSundayOfEachMonth/LastSundayOfEachMonth/LastSundayUnitTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using LastSunday;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastSundayOfEachMonth
+{
+    [TestFixture]
+    public class LastSundayUnitTests
+    {
+       
+         
+        [Test]
+        public void CheckIfInputOfAGivenYear_ReturnsCorrectOutput()
+        {
+            List<DateTime> expected = new List<DateTime>();
+            expected.Add(new DateTime(2019,1,27));
+            expected.Add(new DateTime(2019,2,24));
+            expected.Add(new DateTime(2019,3,31));
+            expected.Add(new DateTime(2019,4,28));
+            expected.Add(new DateTime(2019,5,26));
+            expected.Add(new DateTime(2019,6,30));
+            expected.Add(new DateTime(2019,7,28));
+            expected.Add(new DateTime(2019,8,25));
+            expected.Add(new DateTime(2019,9,29));
+            expected.Add(new DateTime(2019,10,27));
+            expected.Add(new DateTime(2019,11,24));
+            expected.Add(new DateTime(2019,12,29));
+
+            var actual = LastSunday.Services.FindLastSundayOfEachMonth(2019);
+            Assert.AreEqual(expected, actual);
+            //Assert.AreEqual(System.Collections.ICollection expected, System.Collections.ICollection actual, string message);
+        }
+
+        
+    }
+}
