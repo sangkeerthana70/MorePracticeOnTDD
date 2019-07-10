15c525cf4389b8eee714c75328c6bfce2c161e23
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-10 00:02:53 -0400

    Initial commit - setup of Tdd -passed first test case

---
+++ MarsRoverTdd/MarsRover/Rover.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Rover
+    {
+        public int[] location = new int[2];
+        public char direction;
+        public int[] grid = new int[2];
+
+        //public Rover()
+        //{
+        //}
+
+        public Rover(int[] loc, char direction, int[] grid)
+        {
+            this.location = loc;
+            this.direction = direction;
+            this.grid = grid;
+        }
+
+        public void Move(string command)
+        {
+            if(command.Length == 1)
+            {
+                if (command == "e")
+                {
+                    location[1] += 1;
+                }               
+                Console.WriteLine("location " + String.Join(",", location));
+            }
+        }
+    }
+}
---
+++ MarsRoverTdd/MarsRoverTdd/MarsRoverUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using MarsRover;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRoverTdd
+{
+    [TestFixture]
+    public class MarsRoverUnitTests
+    {
+       
+       
+        [Test]
+        public void Command_fMovesRoverToZeroOneFromZeroZero()
+        {
+            Rover rover = new Rover(new int[] { 0, 0 }, 'e', new int[] { 10,10});
+            string command = "e";
+            int[] expectedLocation = new int[] { 0, 1};
+            char expectedDirection = 'e';
+            rover.Move(command);
+
+            Assert.AreEqual(expectedLocation, rover.location);
+            Assert.AreEqual(expectedDirection, rover.direction);
+        }
+
+    }
+}
