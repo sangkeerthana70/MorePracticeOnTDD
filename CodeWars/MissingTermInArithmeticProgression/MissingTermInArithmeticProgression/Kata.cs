e4f77390976c81db820ee61a3a21fb996a78b109
sangkeerthana70 <sangkeerthana70@gmail.com>
2019-07-17 12:37:06 -0400

    initial commit

---
+++ CodeWars/MissingTermInArithmeticProgression/MissingTermInArithmeticProgression/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingTermInArithmeticProgression
+{
+    public class Kata
+    {
+        public static int FindMissing(List<int> list)
+        {
+            //return list[0];
+
+            int difference = list[1] - list[0];
+            int missingNum = 0;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i + 1] - list[i] == difference)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("in else");
+                    Console.WriteLine("list[i+1] " + list[i+1]);
+                    Console.WriteLine("list[i] " + list[i]);
+                    int newDifference = list[i + 1] - list[i];
+                    Console.WriteLine("newDifference " + newDifference);
+                    missingNum = list[i] + (newDifference - difference);
+                    Console.WriteLine("missingNum " + missingNum);
+                }
+
+            }
+            return missingNum;
+        }
+    }
+}
---
+++ CodeWars/MissingTermInArithmeticProgression/MissingTermInArithmeticProgression/UnitTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingTermInArithmeticProgression
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        /*
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(new[] { new List<int> { 1, 3, 5, 9, 11 } }).Returns(7);
+                yield return new TestCaseData(new[] { new List<int> { 0, 5, 10, 20, 25 } }).Returns(15);
+                yield return new TestCaseData(new[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 } }).Returns(10);
+                yield return new TestCaseData(new[] { new List<int> { 1040, 1220, 1580 } }).Returns(1400);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(List<int> list) => Kata.FindMissing(list);
+        */
+        [Test]
+        public void a_IntegerList_ReturnsMissingNumberInSeries()
+        {
+            List<int> ints = new List<int> { 1, 3, 5, 9, 11 };
+            int expected = 7;
+            int actual = Kata.FindMissing(ints);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void b_IntegerList_ReturnsMissingNumberInSeries()
+        {
+            List<int> ints = new List<int> { 0, 5, 10, 20, 25 };
+            int expected = 15;
+            int actual = Kata.FindMissing(ints);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
